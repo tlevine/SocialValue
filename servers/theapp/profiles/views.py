4a190511b669c2bbeecb40e44f6a03778bbcd746
Thomas Levine <thomas.levine@gmail.com>
2011-11-28 17:43:22 -0500

    !!

--- servers/theapp/profiles/views.py
+++ servers/theapp/profiles/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render_to_response
+from annoying.decorators import render_to
 
+@render_to('profile.html')
 def profile(request):
-  return render_to_response('profile.html')
+  return {}
