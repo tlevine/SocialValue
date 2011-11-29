863f4c4e6dfd1d0b30d48caa6bb6cce724319b1b
Thomas Levine <thomas.levine@gmail.com>
2011-11-28 20:57:37 -0500

    keychain

--- servers/theapp/keychain/models.py
+++ servers/theapp/keychain/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from gnupg import GPG
+
+import os
+
+GNUPG_HOME='gpg'
 
 class Keychain(models.Model):
   user=models.OneToOneField(User)
-#  private_key=
+  def gen(this):
+    gpg=GPG(gnupghome=os.path.join(GNUPG_HOME,user.pk))
+    input_data = gpg.gen_key_input(key_type="RSA", key_length=1024)
+    key=gpg.gen_key(input_data)
