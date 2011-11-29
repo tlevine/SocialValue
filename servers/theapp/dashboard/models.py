47f7b82d32189d471fc3d9ad50f684f4c2613909
Thomas Levine <thomas.levine@gmail.com>
2011-11-28 21:08:22 -0500

    transaction models

--- servers/theapp/dashboard/models.py
+++ servers/theapp/dashboard/models.py
@@ -1,3 +1,13 @@
+"Do I want http://packages.python.org/CouchDB/client.html ?"
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Couch(models.Model):
+  "Connection to a Couch"
+  pass
+
+class Archive(models.Model):
+  "A link to a transaction stored in a Couch"
+  couch=models.ForeignKey(Couch)
+  uuid=models.CharField(max_length=100)
+  owner=models.ForeignKey(User)
