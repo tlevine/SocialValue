0d7675852a277f9bee961bd4c023ad968eeaaa36
Thomas Levine <thomas.levine@gmail.com>
2011-11-30 16:42:29 -0500

    add jquery and buttons

--- servers/theapp/settings.py
+++ servers/theapp/settings.py
@@ -73,6 +73,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+  os.path.join(PROJECT_ROOT, "./static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -131,17 +132,17 @@ INSTALLED_APPS = (
   'allauth.socialaccount',
   'allauth.twitter',
   'allauth.openid',
-#  'allauth.facebook',
+# 'allauth.facebook',
 
   'annoying',
 
   #SocialValue
-  'gpg_transactions',
+# 'gpg_transactions',
   'query_identities',
   'query_archives',
 
   #theapp
-  'keychain',
+# 'keychain',
   'dashboard',
   'profiles',
 )
